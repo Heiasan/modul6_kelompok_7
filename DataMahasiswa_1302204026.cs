2ed997a9ae907b576009349b956ef3e02e6c6239
sumryooo <adha.surya2002@gmail.com>
2022-04-02 16:22:46 +0700

    menambahkan class DataMahasiswa

---
+++ DataMahasiswa_1302204026.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace modul6_kelompok_7
+{
+    internal class DataMahasiswa_1302204026
+    {
+        string jsonFilePath = @"C:\Users\ASUS -TUF GAMING\source\repos\modul6_kelompok_7";
+        public void ReadJSON()
+        {
+            string jsonFile = File.ReadAllText(jsonFilePath);
+            dynamic data = JsonConvert.DeserializeObject(jsonFile);
+
+            Console.WriteLine("First Name : " + data.firstName);
+            Console.WriteLine("Last Name : " + data.lastName);
+            Console.WriteLine("Gender : " + data.gender);
+            Console.WriteLine("Age : " + data.age);
+            Console.WriteLine("Alamat : " + data.address.streetAddress + ", " + data.address.city + ", " + data.address.state);
+            Console.WriteLine("Mata Kuliah : ");
+            foreach (var item in data.courses)
+            {
+
+                Console.WriteLine("     Kode matkul : " + item.code);
+                Console.WriteLine("     Nama matkul : " + item.name);
+            }
+            Console.WriteLine();
+        }
+    }
+}
