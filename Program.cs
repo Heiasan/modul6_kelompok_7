49cbe775d09b8ea22dcb71c6ba93616737d0e6a7
Gerry William <gwilliam@student.telkomuniversity.ac.id>
2022-04-02 15:47:33 +0700

    commit pertama

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_kelompok_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
